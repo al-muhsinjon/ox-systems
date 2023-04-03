e30d21369fae8222bfec64fa6bbc7ba4a4e56d92
muhsindev0 <muhsinjonmullajonov0@gmail.com>
2023-04-03 11:05:46 +0500

    ox-systems

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./Home";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter >
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
         <Route path="/" element={<Home2 />} />
